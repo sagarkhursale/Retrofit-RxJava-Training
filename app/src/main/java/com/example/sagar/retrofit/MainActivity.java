9acb42ead880c1c4bc10b9e1593f2d34ca6388a9
sagarkhursale <khursule.sagar@gmail.com>
2019-01-07 19:01:03 +0530

    set adapter

--- app/src/main/java/com/example/sagar/retrofit/MainActivity.java
+++ app/src/main/java/com/example/sagar/retrofit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sagar.retrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,6 +18,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.support.v7.widget.RecyclerView.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static String API_KEY;
     private static final int PAGE_ONE = 1;
+
+    MovieListAdapter mMovieListAdapter;
 
 
     @Override
@@ -32,17 +38,31 @@ public class MainActivity extends AppCompatActivity {
 
         API_KEY = getString(R.string.api_key);
 
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+
+        LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        // Call to MovieApiService
 
         MovieApiService movieApiService = ApiClient.getRetrofitClient().create(MovieApiService.class);
 
-
         Call<TopRated> topRatedMovies = movieApiService.getTopRatedMovies(API_KEY, PAGE_ONE);
-
 
         topRatedMovies.enqueue(new Callback<TopRated>() {
             @Override
             public void onResponse(Call<TopRated> call, Response<TopRated> response) {
                 List<Result> resultList = response.body().getResults();
+
+                mMovieListAdapter = new MovieListAdapter(resultList);
+                recyclerView.setAdapter(mMovieListAdapter);
+
                 for (int i = 1; i < resultList.size(); i++)
                     Log.i(TAG, "\n" + i + ")\t" + resultList.get(i).getTitle());
             }
