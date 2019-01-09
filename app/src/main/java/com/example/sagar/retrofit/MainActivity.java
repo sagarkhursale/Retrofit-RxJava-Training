dd61cc46e7f7a8aef25a49a7b5e87ddcc9942c7b
sagarkhursale <khursule.sagar@gmail.com>
2019-01-09 13:01:22 +0530

    intent togoNext

--- app/src/main/java/com/example/sagar/retrofit/MainActivity.java
+++ app/src/main/java/com/example/sagar/retrofit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sagar.retrofit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -58,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
         topRatedMovies = movieApiService.getTopRatedMovies(API_KEY, PAGE_ONE);
 
-        resultCallback();
+        // resultCallback();
     }
 
 
@@ -90,12 +91,15 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if (id == R.id.gotoNext) {
 
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
 
         }
 
